95748c94ac900d2392af8e087c1059a4ab6a75d0
arkanhakim077 <arkanhakim077@gmail.com>
2026-01-21 17:46:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Website Masjid Al-Ukhuwah berhasil dimuat");
+
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Assalamu’alaikum, selamat datang di website Masjid Al-Ukhuwah");
+});
